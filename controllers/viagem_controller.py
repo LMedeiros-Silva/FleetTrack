1e4a237cb6be3edfe33872f96fa9b11aae1edf02
LMedeiros-Silva <leomedeirossilva4@gmail.com>
2026-04-29 01:46:25 -0300

    Implementaçao do historico de viagem

--- controllers/viagem_controller.py
+++ controllers/viagem_controller.py
@@ -1,9 +1,11 @@
 from models.viagem import Viagem
 from services.previsao_custo import calcular_custo
 
+
 class ViagemController:
     def __init__(self):
         self.viagens = []
+        self.historico_previsoes = []
 
     def criar(self, veiculo, rota, data, preco_combustivel):
         custo = calcular_custo(
@@ -21,4 +23,18 @@ class ViagemController:
         return self.viagens
 
     def prever_custo(self, distancia, consumo, preco_combustivel):
-        return calcular_custo(distancia, consumo, preco_combustivel)
+        custo = calcular_custo(distancia, consumo, preco_combustivel)
+
+        previsao = {
+            "distancia": distancia,
+            "consumo": consumo,
+            "preco_combustivel": preco_combustivel,
+            "custo": custo
+        }
+
+        self.historico_previsoes.append(previsao)
+
+        return custo
+
+    def listar_historico_previsoes(self):
+        return self.historico_previsoes
