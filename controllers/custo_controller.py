dc294b8955ab07a320ddb8d5e73b43f0830cf670
LMedeiros-Silva <leomedeirossilva4@gmail.com>
2026-04-29 02:22:13 -0300

    Atualizaçao do custo_controller.py

--- controllers/custo_controller.py
+++ controllers/custo_controller.py
@@ -1,12 +1,79 @@
 from models.custo import Custo
+from utils.validacoes import validar_texto, validar_float_positivo
+from services.persistencia import Persistencia
+
 
 class CustoController:
-    def __init__(self):
+    def __init__(self, veiculo_controller):
+        self.veiculo_controller = veiculo_controller
         self.custos = []
+        self.carregar_custos()
 
-    def adicionar(self, descricao, valor):
-        custo = Custo(descricao, valor)
+    def carregar_custos(self):
+        dados = Persistencia.carregar("manutencoes.json")
+
+        for item in dados:
+            veiculo = self.buscar_veiculo_por_placa(item["placa_veiculo"])
+
+            if veiculo:
+                custo = Custo(
+                    veiculo,
+                    item["tipo_manutencao"],
+                    item["descricao"],
+                    item["valor"],
+                    item["data"]
+                )
+                self.custos.append(custo)
+
+    def salvar_custos(self):
+        dados = []
+
+        for custo in self.custos:
+            dados.append({
+                "placa_veiculo": custo.veiculo.placa,
+                "tipo_manutencao": custo.tipo_manutencao,
+                "descricao": custo.descricao,
+                "valor": custo.valor,
+                "data": custo.data
+            })
+
+        Persistencia.salvar("manutencoes.json", dados)
+
+    def buscar_veiculo_por_placa(self, placa):
+        for veiculo in self.veiculo_controller.listar():
+            if veiculo.placa == placa:
+                return veiculo
+
+        return None
+
+    def adicionar(self, veiculo, tipo_manutencao, descricao, valor, data):
+        if veiculo is None:
+            return False, "Selecione um veículo válido."
+
+        if not validar_texto(tipo_manutencao):
+            return False, "Tipo de manutenção inválido."
+
+        if not validar_texto(descricao):
+            return False, "Descrição inválida."
+
+        if not validar_float_positivo(valor):
+            return False, "Valor inválido."
+
+        if not validar_texto(data):
+            return False, "Data inválida."
+
+        custo = Custo(
+            veiculo,
+            tipo_manutencao,
+            descricao,
+            float(valor),
+            data
+        )
+
         self.custos.append(custo)
+        self.salvar_custos()
+
+        return True, "Manutenção cadastrada com sucesso."
 
     def listar(self):
         return self.custos
