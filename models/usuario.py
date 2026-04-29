00877ddd4984cc1fa2564453cbcd729cddd67277
LMedeiros-Silva <leomedeirossilva4@gmail.com>
2026-04-29 00:39:16 -0300

    Atualizacao do usuario.py

--- models/usuario.py
+++ models/usuario.py
@@ -1,5 +1,5 @@
-class usuario:
-    def _init(self, nome, email, senha):
+class Usuario:
+    def __init__(self, nome, email, senha):
         self.nome = nome
         self.email = email
         self.senha = senha
