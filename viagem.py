d5ed16fa4e5e274282658af7bcaa15dd9a7f85c4
brennodn <brennodnsp@hotmail.com>
2026-04-28 23:58:59 -0300

    Criacao viagem.py

---
+++ viagem.py
@@ -0,0 +1,6 @@
+class Viagem:
+    def __init__(self, veiculo, rota, data, custo_previsto):
+        self.veiculo = veiculo
+        self.rota = rota
+        self.data = data
+        self.custo_previsto = custo_previsto
