be599d5a744ecb60e2531054b9e6ffc2f1ae687c
LMedeiros-Silva <leomedeirossilva4@gmail.com>
2026-04-29 02:21:35 -0300

    Atualização do rota_controller

--- controllers/rota_controller.py
+++ controllers/rota_controller.py
@@ -1,9 +1,35 @@
 from models.rota import Rota
 from utils.validacoes import validar_texto, validar_float_positivo
+from services.persistencia import Persistencia
+
 
 class RotaController:
     def __init__(self):
         self.rotas = []
+        self.carregar_rotas()
+
+    def carregar_rotas(self):
+        dados = Persistencia.carregar("rotas.json")
+
+        for item in dados:
+            rota = Rota(
+                item["origem"],
+                item["destino"],
+                item["distancia"]
+            )
+            self.rotas.append(rota)
+
+    def salvar_rotas(self):
+        dados = []
+
+        for rota in self.rotas:
+            dados.append({
+                "origem": rota.origem,
+                "destino": rota.destino,
+                "distancia": rota.distancia
+            })
+
+        Persistencia.salvar("rotas.json", dados)
 
     def cadastrar(self, origem, destino, distancia):
         if not validar_texto(origem):
@@ -17,6 +43,7 @@ class RotaController:
 
         rota = Rota(origem, destino, float(distancia))
         self.rotas.append(rota)
+        self.salvar_rotas()
 
         return True, "Rota cadastrada com sucesso."
 
