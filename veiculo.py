d03ca809a3cefa9177623ac1125134ff00618d37
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-28 21:03:02 -0300

    Criação do veiculo.py

---
+++ veiculo.py
@@ -0,0 +1,7 @@
+class Veiculo:
+    def __init__(self, placa, modelo, tipo, capacidade, consumo):
+        self.placa = placa
+        self.modelo = modelo
+        self.tipo = tipo
+        self.capacidade = capacidade
+        self.consumo = consumo
